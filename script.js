4aad63903c65a2164143e56360713a886592cf37
Matakady <matejkaderabe@gmail.com>
2022-03-23 17:46:47 +0100

    random generator

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
 const zelenina = [
-    'rajče',
-    'okurka',
-    'paprika',
-    'mrkev',
-    'brambory',
-    'celer',
-    'zelí',
-    'květák',
-    'ředkvičky',
-    'brokolice'
-]
+  "rajče",
+  "okurka",
+  "paprika",
+  "mrkev",
+  "brambory",
+  "celer",
+  "zelí",
+  "květák",
+  "ředkvičky",
+  "brokolice",
+];
 
-const ovoce = [
-    'jablko',
-    'hruška',
-    'jahody',
-    'třešně'
-]
+const ovoce = ["jablko", "hruška", "jahody", "třešně"];
 
 const vety = [
-   "Zůstaň stát",
-   "Přeskoč o dvě pole dopředu",
-   "Dej se doprava",
-   "Dej se doleva",
-   "Jdi rovně",
-   "Jedno kolo stojíš" 
-]
+  "Zůstaň stát",
+  "Přeskoč o dvě pole dopředu",
+  "Dej se doprava",
+  "Dej se doleva",
+  "Jdi rovně",
+  "Jedno kolo stojíš",
+];
 
+const days = [
+  "Monday",
+  "Tuesday",
+  "Wednessday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const kostka = document.querySelector("h3");
+const tlacitko = document.querySelector("button");
+tlacitko.onclick = () => {
+  const nahodneCislo = Math.floor(Math.random() * 6) + 1;
+  kostka.textContent = nahodneCislo;
+};
+
+//console.log(days.length);
+//days[2] = "It is Wednessday, my dudes";
+//console.log(days[2]);
 
 // index
 // length
@@ -37,6 +52,3 @@ const vety = [
 // includes
 // sort
 // Math.random()
-
-
-
